53eea968629651aab5f1efe658a881d17f1bf23c
HOME\hae96 <gusdn200@gmail.com>
2024-02-07 09:38:28 +0900

    Field 변경, Sub Scene셋팅 변경, NavMeshAgent 작성 테스트 코드

--- Assets/02.Scriptes/ECS/Aspect/NavAgentAspect.cs
+++ Assets/02.Scriptes/ECS/Aspect/NavAgentAspect.cs
@@ -12,17 +12,58 @@ namespace Game.Ecs
         private readonly RefRW<NavAgentProperties> _navAgentProperties;
         private readonly DynamicBuffer<WaypointBuffer> _waypointBuffer;
 
+        public float3 Position {
+            get { return _localTransform.ValueRO.Position; }
+            set { _localTransform.ValueRW.Position = value; }
+        }
+        public quaternion Rotation {
+            get { return _localTransform.ValueRO.Rotation; }
+            set { _localTransform.ValueRW.Rotation = value; }
+        }
+        public float Timer {
+            get { return _navAgentProperties.ValueRO.timer; }
+            set { _navAgentProperties.ValueRW.timer = value; }
+        }
         public bool IsStop() {
             return _navAgentProperties.ValueRO.isStop;
         }
-
-        public float3 GetPosition() {
-            return _localTransform.ValueRO.Position;
+        public bool IsFinded() {
+            return _navAgentProperties.ValueRO.isPathFinded;
         }
 
         public Entity GetTargetEntity() {
-
             return _navAgentProperties.ValueRO.targetEntity;
+        }
+
+        public float GetTimer() {
+            return _navAgentProperties.ValueRO.timer;
+        }
+        
+        public float GetMoveSpeed() {
+            return _navAgentProperties.ValueRO.moveSpeed;
+        }
+        
+        /// <summary>
+        /// 검색 완료 후 Nav Properties 리셋
+        /// </summary>
+        public void FindedNavAgent() {
+            _navAgentProperties.ValueRW.curretWaypoint = 0;
+            _navAgentProperties.ValueRW.isPathFinded = true;
+        }
+
+        public void ClearWaypointBuffer() {
+            _waypointBuffer.Clear();
+        }
+        public void AddWaypointBuffer(float3 position) {
+            _waypointBuffer.Add(new WaypointBuffer { waypoint = position });
+        }
+        public float3 GetCurrentWaypointPosition() {
+            return _waypointBuffer[_navAgentProperties.ValueRO.curretWaypoint].waypoint;
+        }
+        public void NextWaypoint() {
+            if(_navAgentProperties.ValueRO.curretWaypoint + 1 < _waypointBuffer.Length) {
+                _navAgentProperties.ValueRW.curretWaypoint++;
+            }
         }
     }
 }
--- Assets/02.Scriptes/ECS/Systems/NavAgentSystem.cs
+++ Assets/02.Scriptes/ECS/Systems/NavAgentSystem.cs
@@ -7,36 +7,24 @@ using UnityEngine.Experimental.AI;
 using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Transforms;
+
 namespace Game.Ecs {
     /// <summary>
     /// Nav Agent 컨트롤 시스템
     /// </summary>
     [BurstCompile]
     public partial struct NavAgentSystem : ISystem {
-        private int count;
-        [BurstCompile]
-        private partial struct NavAgentMoveJob : IJobEntity {
-            public float deltaTime;
- 
-            [BurstCompile]
-            private void Execute(NavAgentAspect aspect) {
-                if (aspect.IsStop()) return;
 
-
-            }
-            [BurstCompile]
-            private void Move(NavAgentAspect aspect) {
-                // 회전
-
-                // 이동
-
-            }
-            
-        }
-        
+        private NavMeshQuery navQuery;
+        private float queryTimer;
+        private float queryUpdateTime;
+        private float findUpdateTime;
         [BurstCompile]
         private void OnCreate(ref SystemState state) {
-            count = 0;
+            CreateNavMesh();
+            queryTimer = 0;
+            queryUpdateTime = 1;
+            findUpdateTime = 0.5f;
         }
         [BurstCompile]
         private void OnDestroy(ref SystemState state) {
@@ -45,29 +33,121 @@ namespace Game.Ecs {
         [BurstCompile]
         private void OnUpdate(ref SystemState state) {
             float deltaTime = SystemAPI.Time.DeltaTime;
+            UpdateNavMeshQuery(deltaTime);
+            FindPath(ref state, deltaTime);
 
-            //new NavAgentMoveJob {
-            //    deltaTime = deltaTime
-            //}.ScheduleParallel();
-            count++;
-            FindPath(ref state);
-
+            new NavAgentMoveJob {
+                deltaTime = deltaTime 
+            }.ScheduleParallel();
         }
 
- //       [BurstCompile]
-        private void FindPath(ref SystemState state) {
-            if (count > 1) return;
-            var navQuery = new NavMeshQuery(NavMeshWorld.GetDefaultWorld(), Allocator.TempJob, 1000);
+        #region Method and Jobs
 
+        private void CreateNavMesh() {
+            navQuery = new NavMeshQuery(NavMeshWorld.GetDefaultWorld(), Allocator.Persistent, 1000);
+        }
+        private void UpdateNavMeshQuery(float deltaTime) {
+            queryTimer += deltaTime;
+            if (queryTimer > queryUpdateTime) {
+                queryTimer = 0;
+                navQuery.Dispose();
+                CreateNavMesh();
+            }
+        }
+        [BurstCompile]
+        private void FindPath(ref SystemState state, float deltaTime) {
+            float3 extents = new float3(1, 3, 1);
+            int maxPathSize = 100;
             foreach (var navAspect in SystemAPI.Query<NavAgentAspect>()) {
-                float3 fromPos = navAspect.GetPosition();
-                float3 toPos = SystemAPI.GetComponent<LocalTransform>(navAspect.GetTargetEntity()).Position;
-                Debug.Log(toPos);
+                // 업데이트 간격
+                navAspect.Timer += deltaTime;
+                if (navAspect.Timer < findUpdateTime) continue;
+                navAspect.Timer = 0f;
+                // 검색
+                float3 startPosition = navAspect.Position;
+                float3 endPosition = SystemAPI.GetComponent<LocalTransform>(navAspect.GetTargetEntity()).Position;
+
+                NavMeshLocation startLocation = navQuery.MapLocation(startPosition, extents, 0);
+                NavMeshLocation endLocation = navQuery.MapLocation(endPosition, extents, 0);
+                PathQueryStatus status;
+                PathQueryStatus returningStatus;
+                Debug.Log(navQuery.IsValid(startLocation) + " " + navQuery.IsValid(endLocation));
+                if (navQuery.IsValid(startLocation) && navQuery.IsValid(endLocation)) {
+                    Debug.Log("Chk");
+                    status = navQuery.BeginFindPath(startLocation, endLocation);
+                    if(status == PathQueryStatus.InProgress) {
+                        status = navQuery.UpdateFindPath(maxPathSize, out int iterationsPerformed);
+                        if(status == PathQueryStatus.Success) {
+                            status = navQuery.EndFindPath(out int pathSize);
+
+                            NativeArray<NavMeshLocation> result = new NativeArray<NavMeshLocation>(pathSize + 1, Allocator.Temp);
+                            NativeArray<StraightPathFlags> straightPathFlags = new NativeArray<StraightPathFlags>(maxPathSize, Allocator.Temp);
+                            NativeArray<float> vertexSize = new NativeArray<float>(maxPathSize, Allocator.Temp);
+                            NativeArray<PolygonId> polygonIds = new NativeArray<PolygonId>(pathSize + 1, Allocator.Temp);
+                            int straightPathCount = 0;
+
+                            navQuery.GetPathResult(polygonIds);
+
+                            returningStatus = PathUtils.FindStraightPath(
+                                navQuery,
+                                startPosition,
+                                endPosition,
+                                polygonIds,
+                                pathSize,
+                                ref result,
+                                ref straightPathFlags,
+                                ref vertexSize,
+                                ref straightPathCount,
+                                maxPathSize
+                                );
+                            if (returningStatus == PathQueryStatus.Success) {
+                                navAspect.ClearWaypointBuffer();
+
+                                foreach(NavMeshLocation location in result) {
+                                    if(location.position != Vector3.zero) {
+                                        navAspect.AddWaypointBuffer(location.position);
+                                    }
+                                }
+                                navAspect.FindedNavAgent();
+                            }
+                            straightPathFlags.Dispose();
+                            polygonIds.Dispose();
+                            vertexSize.Dispose();
+                        }
+                        
+                    }
+                }
+            }   
+        }
+        /// <summary>
+        /// Move Job
+        /// </summary>
+        [BurstCompile]
+        private partial struct NavAgentMoveJob : IJobEntity {
+            public float deltaTime;
+
+            [BurstCompile]
+            private void Execute(NavAgentAspect aspect) {
+                if (aspect.IsStop()) return;
+                if (aspect.IsFinded()) {
+                    Move(aspect);
+                }
+            }
+            [BurstCompile]
+            private void Move(NavAgentAspect aspect) {
+                if (math.distance(aspect.Position, aspect.GetCurrentWaypointPosition()) < 0.3f) {
+                    aspect.NextWaypoint();
+                }
+                // 회전
+                float3 direction = aspect.GetCurrentWaypointPosition() - aspect.Position;
+                float angle = math.degrees(math.atan2(direction.z, direction.x));
+                aspect.Rotation = math.slerp(aspect.Rotation, quaternion.Euler(new float3(0, angle, 0)), deltaTime);
+                // 이동
+                aspect.Position += math.normalize(direction) * aspect.GetMoveSpeed() * deltaTime;
             }
 
-            navQuery.Dispose();
-
         }
+        #endregion
     }
 }
 
